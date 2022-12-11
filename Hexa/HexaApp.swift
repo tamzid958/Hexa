462c7352c9d8d640ff0fa6b4a5a256586266679d
Tamzid Ahmed <tamzid.ahmed@dsinnovators.com>
2022-12-11 21:25:33 +0600

    Initial Commit

---
+++ Hexa/HexaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexaApp.swift
+//  Hexa
+//
+//  Created by Tamzid Ahmed on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HexaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
